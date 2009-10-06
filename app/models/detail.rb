a047a1b7ccfdbc4a9d993f4edf4bdadeb53deecb
Mike Schwab <mike.schwab@gmail.com>
2009-10-05 23:43:24 -0400

    can view private details for own account

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -3,4 +3,5 @@ class Detail < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User'
   
   named_scope :public, {:conditions => ['details.private > 0']}
+  named_scope :private, {:conditions => ['details.private = 0']}
 end
