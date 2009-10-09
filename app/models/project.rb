96c37facc0c5e7827f07067802e0cd054872b3a0
Mike Schwab <mike.schwab@gmail.com>
2009-10-08 22:56:36 -0400

    express interest in projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,6 @@ class Project < ActiveRecord::Base
   has_many :calls
   has_many :roles, :through => :calls
   has_many :details, :as => :subject
+  has_many :interests, :as => :subject
+  has_many :interested_users, :through => :interests, :source => :user
 end
